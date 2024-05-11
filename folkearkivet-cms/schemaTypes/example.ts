4eda10b3a8a06da365fc86f673a422cf085f8d9c
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-05-11 13:36:18 +0200

    add sanity cms

---
+++ folkearkivet-cms/schemaTypes/example.ts
@@ -0,0 +1,15 @@
+import {defineType, defineField, defineArrayMember} from 'sanity'
+
+export const example = defineType({
+  title: 'Some Document Type',
+  name: 'exampleDocumentType',
+  type: 'document',
+  fields: [
+    defineField({
+      title: 'String array',
+      name: 'strings',
+      type: 'array',
+      of: [defineArrayMember({type: 'string'})],
+    }),
+  ],
+})
