3b7e7fd667aa9b4cf7f8867a5b3fc08dd56ebe22
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2025-12-22 11:30:02 +0100

    add check for missing images on contribution

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -89,47 +89,50 @@ const Archive = () => {
 				)}
 			</div>
 			<ul className="photo-gallery">
-				{archive.map((contribution, index) => (
-					<li key={contribution._id + "-" + index} className="relative group mb-[12px]">
-						<div className="relative w-full mb-2 overflow-hidden">
-							<img
-								width={400}
-								height={400}
-								className="invisible"
-								alt=""
-								aria-hidden="true"
-								src={contribution.image.asset.metadata.lqip}
-							/>
-							{!loadedImages[contribution._id] && <div className="absolute inset-0 bg-gray-200 animate-pulse" />}
-							<Link to={`/arkivet/${contribution._id}`}>
-								<img
-									className={classNames(
-										"absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-50",
-										{
-											"opacity-0": !loadedImages[contribution._id],
-											"opacity-100": loadedImages[contribution._id],
-										},
-									)}
-									width={400}
-									height={400}
-									srcSet={[
-										`${urlFor(contribution.image).width(300).auto("format").url()} 300w`,
-										`${urlFor(contribution.image).width(600).auto("format").url()} 600w`,
-										`${urlFor(contribution.image).width(900).auto("format").url()} 900w`,
-									].join(", ")}
-									sizes="(max-width: 640px) 50vw, 20vw"
-									alt={contribution.title}
-									onLoad={() => {
-										setLoadedImages((prev) => ({
-											...prev,
-											[contribution._id]: true,
-										}));
-									}}
-								/>
-							</Link>
-						</div>
-					</li>
-				))}
+				{archive.map(
+					(contribution, index) =>
+						contribution.image && (
+							<li key={contribution._id + "-" + index} className="relative group mb-[12px]">
+								<div className="relative w-full mb-2 overflow-hidden">
+									<img
+										width={400}
+										height={400}
+										className="invisible"
+										alt=""
+										aria-hidden="true"
+										src={contribution.image.asset.metadata.lqip}
+									/>
+									{!loadedImages[contribution._id] && <div className="absolute inset-0 bg-gray-200 animate-pulse" />}
+									<Link to={`/arkivet/${contribution._id}`}>
+										<img
+											className={classNames(
+												"absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-50",
+												{
+													"opacity-0": !loadedImages[contribution._id],
+													"opacity-100": loadedImages[contribution._id],
+												},
+											)}
+											width={400}
+											height={400}
+											srcSet={[
+												`${urlFor(contribution.image).width(300).auto("format").url()} 300w`,
+												`${urlFor(contribution.image).width(600).auto("format").url()} 600w`,
+												`${urlFor(contribution.image).width(900).auto("format").url()} 900w`,
+											].join(", ")}
+											sizes="(max-width: 640px) 50vw, 20vw"
+											alt={contribution.title}
+											onLoad={() => {
+												setLoadedImages((prev) => ({
+													...prev,
+													[contribution._id]: true,
+												}));
+											}}
+										/>
+									</Link>
+								</div>
+							</li>
+						),
+				)}
 			</ul>
 		</Layout>
 	);
